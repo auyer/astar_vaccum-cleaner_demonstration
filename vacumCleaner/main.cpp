9d69928000439c00b65be5901f03e63c816d68be
Rafael Passos <rafa_auyer@icloud.com>
2016-08-29 11:46:42 -0300

    Initial Commit

---
+++ vacumCleaner/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  vacumCleaner
+//
+//  Created by Rafael Auyer on 29/08/16.
+//  Copyright © 2016 Rafael Auyer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
